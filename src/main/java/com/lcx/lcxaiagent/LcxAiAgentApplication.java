21ad037b809d75b0ac1db24778549cbb4bef201d
lcx <hello@lcx.cn>
2026-01-11 00:25:01 +0800

    初始化项目-运行成功

---
+++ src/main/java/com/lcx/lcxaiagent/LcxAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lcx.lcxaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcxAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcxAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcx/lcxaiagent/LcxAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcx.lcxaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcxAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
